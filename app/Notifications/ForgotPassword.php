0d7553d2c9364218a180a38592c937d864391223
Jesús Arriaga <jarriagabarron@gmail.com>
2016-09-13 23:09:05 -0500

    Mail notification and reset password token process

---
+++ app/Notifications/ForgotPassword.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Notifications;
+
+use App\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class ForgotPassword extends Notification
+{
+    use Queueable;
+
+    private $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        $url = url('/reset-password/'.$this->user->token);
+
+        return (new MailMessage)
+                    ->subject('Reset your password')
+                    ->greeting('Reset password instructions')
+                    ->line('Hi '.$this->user->name.' you recently requested a password reset for your account. To complete the process, click the button below.')
+                    ->action('Reset password', $url)
+                    ->line('Thank you for using WoofnRoof!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
--- app/User.php
+++ app/User.php
@@ -15,7 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','facebookId','birthday','latitude','longitude','mobile'
+        'name', 'email', 'password','facebookId','birthday','latitude','longitude','mobile','token'
     ];
 
     /**
@@ -24,6 +24,6 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token'
+        'password', 'remember_token','token'
     ];
 }
