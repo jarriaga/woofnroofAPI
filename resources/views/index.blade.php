897d286977348f1574a7179a95795532356ce8bb
Jesús Arriaga <jarriagabarron@gmail.com>
2016-09-14 14:35:06 -0500

    GA

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,19 @@
+<html>
+<head>
+
+</head>
+<body>
+
+<script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-84128390-1', 'auto');
+    ga('send', 'pageview');
+
+</script>
 <style>
     .center{
         position: absolute;
@@ -24,8 +40,12 @@
         }
     }
 </style>
+
 <link href="https://fonts.googleapis.com/css?family=Baloo+Tamma" rel="stylesheet">
 <div class="center">
     <img src="/images/logo-orange.png" class="image-logo">
     <h1 class="title1">WoofnRoof</h1>
 </div>
+
+</body>
+</html>
